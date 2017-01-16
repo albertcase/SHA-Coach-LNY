75b7aa887b075597dbeed1c5e722c495e83a4b45
vincenwen <vincen.wen@samesamechina.com>
2017-01-16 18:25:14 +0800

    wechat back bug update OK!;

--- template/second.tpl.php
+++ template/second.tpl.php
@@ -226,19 +226,15 @@
     pfun.loadingFnDoing(allimg, function(){
 
         var _cookieTimes = pfun.getCookie("times");
-        alert(_cookieTimes);
         if(_cookieTimes == 2){   // 没有游戏次数时返回禁用
-            alert(6);
             location.href = "/third?type=failure";
         }else if(_cookieTimes == 1){
-            alert(7);
             if(attention){
                 location.href = "/third?type=yattention";
             }else{
                 location.href = "/third?type=nattention";
             }  
         }else{
-            alert(9);
             $(".loading").css({"visibility": "hidden"});
             _lny.init();
             pfun.init();
