5acfbddf7cc723106ba89691759248033c7f8c5a
vincenwen <vincen.wen@samesamechina.com>
2017-01-16 12:41:54 +0800

    ;

--- template/second.tpl.php
+++ template/second.tpl.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>COACH</title>
+    <title>2017“酉”鸿运！</title>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta name="format-detection" content="telephone=no">
     <!-- uc强制竖屏 -->
